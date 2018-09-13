1c884ef3fa6d21a980fb246064d58602442263ea
Willian Welbert <willianwelbert@yahoo.com.br>
2018-09-13 14:51:03 -0300

    ever-b-tru resolved

--- ever-B-tru.js
+++ ever-B-tru.js
@@ -1,19 +1,19 @@
 function truthCheck(collection, pre) {
   // Is everyone being true?
+  let resultVar = true;
 
   //map first arg
-  //Object.keys && Object.values are arrays
+  collection.map(function(item) {
   //map them to verify both != false
+    if(!item[pre]){
+      console.log("false", item.user);
+      return resultVar = false;
+    } 
+   
+  });
 
-  return pre;
+  return resultVar;
+
 }
 
-truthCheck(
-  [
-    { user: "Tinky-Winky", sex: "male" },
-    { user: "Dipsy", sex: "male" },
-    { user: "Laa-Laa", sex: "female" },
-    { user: "Po", sex: "female" }
-  ],
-  "sex"
-);
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")
